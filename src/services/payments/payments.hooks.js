e7f7d806940681d0866e26c656df22e069b8b8a1
Carlo Gammarota <carlo.gammarota@gmail.com>
2023-05-13 20:23:39 -0300

    update

--- src/services/payments/payments.hooks.js
+++ src/services/payments/payments.hooks.js
@@ -9,28 +9,38 @@ module.exports = {
     update: [],
     patch: [
       async context => {
-        console.log('Generar entrada hook', context.data.id_user);
+        let id = context.data.id_user.toString();
+        console.log('Generar entrada hook', id);
         //si el pago esta aprobado se genera la entrada una vez sola de lo contrario no se genera
-        
-        //comprobar si la entrada ya fue generada para no generarla de nuevo
-        let entrada = await context.app.service('entradas').find({ 
-          query: {
-            id_pago: context.data.id_user
-          }
-        });
 
-        if(entrada.total == 0){
-          console.log('Entrada no generada');
-          //crear servicio generar entrada y que devuelva el id de la entrada
-          let entrada = await context.app.service('entradas').create({
-            dni: null,
-            estado: 'no-ingreso',
-            consumision: true,
-            id_pago: context.data.id_user
+        //comprobar si el pago esta aprobado
+        let pago = await context.app.service('payments').get(id);
+        console.log('pago', pago);
+        if(pago.estado == 'aprobado'){
+          console.log('El pago esta aprobado');
+
+        
+          //comprobar si la entrada ya fue generada para no generarla de nuevo
+          let entrada = await context.app.service('entradas').find({ 
+            query: {
+              id_pago: id
+            }
           });
-        }else{
-          console.log('Entrada ya generada');
+
+          if(entrada.total == 0){
+            console.log('Entrada no generada');
+            //crear servicio generar entrada y que devuelva el id de la entrada
+            let entrada = await context.app.service('entradas').create({
+              dni: null,
+              estado: 'no-ingreso',
+              consumision: true,
+              id_pago: id
+            });
+          } else{
+            console.log('Entrada ya generada');
+          }
         }
+        
         
 
         return context;
